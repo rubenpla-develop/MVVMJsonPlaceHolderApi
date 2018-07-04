f93753e699a5b9338d516e6ce933c2aff075c7a5
Ruben Pla <rubenpla.develop@gmail.com>
2018-07-04 17:35:16 +0200

    [GRADLE] - Dagger 2 dependencies added [DI/MODULE] - Dagger 2 NetworkModule class created

---
+++ app/src/main/java/com/develop/rubenpla/mvvmtest/di/module/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.develop.rubenpla.mvvmtest.di.module
+
+import com.develop.rubenpla.mvvmtest.network.PostApi
+import com.develop.rubenpla.mvvmtest.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+/**
+ * Module which provides all required dependencies about network
+ */
+
+@Module
+@Suppress("unused")
+object NetworkModule {
+
+    /**
+     * Provides the Post service implementation.
+     * @param retrofit the Retrofit object used to instantiate the service
+     * @return the Post service implementation.
+     */
+
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun providesPostApi(retrofit : Retrofit) : PostApi {
+
+        return retrofit.create(PostApi::class.java)
+    }
+
+    /**
+     * Provides the Retrofit object.
+     * @return the Retrofit object
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofirInterface() : Retrofit {
+
+        return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory
+                        .createWithScheduler(Schedulers.io()))
+                .build()
+    }
+}
